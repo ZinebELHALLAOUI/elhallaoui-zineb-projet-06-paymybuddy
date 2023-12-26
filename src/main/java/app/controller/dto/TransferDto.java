9ba5e0093adf8fa052bd31d305419105aed9c5d5
ZinebELHALLAOUI <zinebhellaoui@gmail.com>
2023-12-26 21:38:40 +0100

    remove description

--- src/main/java/app/controller/dto/TransferDto.java
+++ src/main/java/app/controller/dto/TransferDto.java
@@ -7,7 +7,6 @@ import java.math.BigDecimal;
 @Data
 public class TransferDto {
     private String connection;
-    private String description = "Fake description";
     private BigDecimal money;
     private String date;
 }
